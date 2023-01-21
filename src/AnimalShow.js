60fd689c1aec0c19f66eb1b50c58668247c24157
Anil <anilkumarthakur60@gmail.com>
2023-01-21 18:01:46 +0545

    update

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -2,9 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function AnimalShow() {
+
+    const handleClick = () => {
+        console.log("Button Clicked");
+    }
     return (
         <div className="App">
             <h1>Animal Show</h1>
+            <button className="btn btn-sm btn-danger" onClick={handleClick()}>Click Me</button>
         </div>
     );
 }
---
+++ src/Section4.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+export  default function Section4() {
+    return (
+        <div className="App">
+            <h1>Section4</h1>
+        </div>
+    );
+}
