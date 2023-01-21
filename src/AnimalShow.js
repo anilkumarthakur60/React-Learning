353bc079f59631029b4413d3c9d406d7265cb851
Anil <anilkumarthakur60@gmail.com>
2023-01-21 15:31:20 +0545

    update

---
+++ src/AnimalShow.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function AnimalShow() {
+    return (
+        <div className="App">
+            <h1>Animal Show</h1>
+        </div>
+    );
+}
+export default AnimalShow;
--- src/App.js
+++ src/App.js
@@ -1,54 +1,14 @@
-import React, {useEffect} from "react";
-import ReactDOM from "react-dom/client";
-import App1, {message1}  from "./App1";
+import React from "react";
 
+import AnimalShow from "./AnimalShow";
 
- function App() {
-    let message = "How are you?";
-    if (Math.random() > 0.5) {
-        message = "I'm fine";
-    }
+function App() {
 
-    const m1 = [
-        {id: 1, name: "John"},
-        {id: 2, name: "Mary"},
-        {id: 3, name: "Peter"}
-
-    ];
-
-
-    let dates = new Date().toLocaleTimeString()
-    useEffect(() => {
-            setInterval(() => {
-                dates = new Date().toLocaleTimeString()
-            }, 1000)
-        }
-    );
     return <>
         <div className="">
-            <div>
-                {message}
-                {m1.map((item, index) => {
-                        return <div key={index}>{item.name}</div>
-                    }
-                )}
-
-                <h1>{
-                    dates
-
-                }</h1>
-            </div>
-            <input autoFocus={true} type={`number`} style={{width: "500px"}} min={12} max={25}/>
-            <br/>
-            <br/>
-
-            <textarea autoFocus={true} style={{width: "500px"}} maxLength={244} minLength={10}/>
-            <br/>
-            <br/>
-            <input spellCheck title={`jdsahf jhdshf`} type={`text`} />
-            {message1}
-            {App1}
+            <AnimalShow/>
         </div>
     </>;
 }
-export  default App;
+
+export default App;
