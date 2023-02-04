58a5e65d96bfdfcfb41d26668a24b93681ec4796
Anil <anilkumarthakur60@gmail.com>
2023-02-05 05:21:31 +0545

    update

--- src/Section10/Section10.js
+++ src/Section10/Section10.js
@@ -1,16 +1,50 @@
 import React from "react";
 import Button from "../Component/Button";
-
+import { GoBell, GoCloudDownload, GoDatabase } from 'react-icons/go';
 
 function Section10() {
+    const handleClick = () => {
+        console.log("Button Clicked");
+    }
     return (
         <div>
-                <div className="">
-                    <Button primary secondary danger  variation={` variation akjsnas`} >aljs</Button>
-                </div>
-
+            <div>
+                <Button
+                    secondary
+                    outline
+                    rounded
+                    className="mb-5"
+                    onClick={handleClick}
+                >
+                    <GoBell />
+                    Click me!!
+                </Button>
+            </div>
+            <div>
+                <Button danger outline onMouseEnter={handleClick}>
+                    <GoCloudDownload />
+                    Buy Now!
+                </Button>
+            </div>
+            <div>
+                <Button warning onMouseLeave={handleClick}>
+                    <GoDatabase />
+                    See Deal!
+                </Button>
+            </div>
+            <div>
+                <Button secondary outline className={`text-white`}>
+                    Hide Ads!
+                </Button>
+            </div>
+            <div>
+                <Button primary rounded>
+                    Something!
+                </Button>
+            </div>
 
         </div>
     )
 }
-export  default  Section10;
+
+export default Section10;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import 'bootstrap/dist/js/bootstrap.bundle.min';
-// import 'bootstrap-icons/font/bootstrap-icons.css';
-
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import './index.css';
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
