1c822a4356bfada1277bec57992c216eb09cdc81
Anil <anilkumarthakur60@gmail.com>
2023-02-12 00:36:10 +0545

    update

--- src/Component/SortableTable.js
+++ src/Component/SortableTable.js
@@ -1,10 +1,29 @@
-
 import Table from "./Table";
+import {useState} from "react";
 
 function SortableTable(props) {
+    const [sortOrder, setSortOrder] = useState(null)
+    const [sortBy, setSortBy] = useState(null)
 
-    const {config} = props;
-    const  updatedConfig = config.map((item) => {
+    const handleClick = (label) => {
+        if (sortOrder === null) {
+            setSortOrder('asc')
+            setSortBy(label)
+        } else if (sortOrder === 'asc') {
+            setSortOrder('desc')
+            setSortBy(label)
+        } else if (sortOrder === 'desc') {
+
+            setSortOrder(null)
+            setSortBy(null)
+            // setSortOrder('asc')
+            // setSortBy(label)
+        }
+    }
+
+
+    const {config, data} = props;
+    const updatedConfig = config.map((item) => {
         if (item.sortValue) {
             return {
                 ...item,
@@ -22,13 +41,38 @@ function SortableTable(props) {
         return item;
     });
 
-    const handleClick = (name) => {
-        console.log(name);
+    // ony sort if sortOrder && sortBy is not null
+    //make a copy of the data
+    //find the correct sortValue function and use it to sort the copy
+    //sort the copy
+    //pass the copy to the table
+
+    let sortedData = data;
+    if (sortOrder && sortBy) {
+        const {sortValue} = config.find((item) => item.label === sortBy);
+        sortedData = [...data.sort((a, b) => {
+            const valueA = sortValue(a);
+            const valueB = sortValue(b);
+            const reverseOrder = sortOrder === 'asc' ? 1 : -1;
+
+            if(typeof valueA === 'string') {
+                return valueA.localeCompare(valueB) * reverseOrder;
+            }else {
+                return (valueA - valueB) * reverseOrder;
+            }
+
+
+        })
+        ]
     }
 
 
     return (
-        <Table {...props} config={updatedConfig}></Table>
+        <div>
+            {sortOrder}-{sortBy}
+
+            <Table {...props} data={sortedData} config={updatedConfig}></Table>
+        </div>
     );
 }
 
