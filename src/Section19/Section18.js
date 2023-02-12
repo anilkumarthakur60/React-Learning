898b94aac1d526b6ee3020b63eef099c49e5343f
Anil <anilkumarthakur60@gmail.com>
2023-02-12 21:17:40 +0545

    update

---
+++ src/Section19/Section18.js
@@ -0,0 +1,13 @@
+import CounterPage19 from "./CounterPage19";
+import CounterPage191 from "./CounterPage191";
+
+function  Section18(){
+    return (
+        <div>
+            <h1>Section 18</h1>
+            <CounterPage19 initialCount={0}></CounterPage19>
+            <CounterPage191 initialCount={0}></CounterPage191>
+        </div>
+    );
+}
+export  default Section18
