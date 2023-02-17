ee4a9d145a377c8d2f25d6fe2d4335a1a83b3a46
Anil <anilkumarthakur60@gmail.com>
2023-02-17 13:06:04 +0545

    update

--- src/Section20/Component/CarSearch.js
+++ src/Section20/Component/CarSearch.js
@@ -1,11 +1,45 @@
-function CarSearch() {
-    return (
-        <div className="bg-purple-50 h-full w-full">
-            <div className="p-2 ">
+import Button from "../../Component/Button";
+import {useState} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {changeSearchTerm} from "../../store";
 
-                lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+function CarSearch() {
+
+    const [search, setSearch] = useState('');
+
+    const searchStateData = useSelector((state) => {
+        return state.cars.searchTerms;
+    });
+
+    const dispatch = useDispatch();
+
+    const formSubmit = (e) => {
+        e.preventDefault();
+        // dispatch(searchCarItem(search));
+        dispatch(changeSearchTerm(search));
+
+
+    }
+
+    const handleChanges = (e) => {
+        setSearch(e.target.value)
+
+        dispatch(changeSearchTerm(e.target.value));
+    }
+
+    return (
+        <form onSubmit={formSubmit}>
+            <div className="flex flex-row space-y-2 my-5">
+                <input type="text"
+                       className="px-2 py-2 border rounded-md focus:outline-none focus:border-blue-400"
+                       placeholder="Search Car..."
+                       value={searchStateData}
+                       name="name"
+                       onChange={handleChanges}
+                />
+                <Button primary type="submit">Search</Button>
             </div>
-        </div>
+        </form>
     )
 }
 
