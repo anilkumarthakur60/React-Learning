3603aefae802402de831c69e2de642ce8840299a
Anil <anilkumarthakur60@gmail.com>
2023-02-17 13:48:38 +0545

    update

--- src/Section20/Component/CarValue.js
+++ src/Section20/Component/CarValue.js
@@ -1,10 +1,36 @@
+import React from 'react'
+import {useSelector} from "react-redux";
+
 function CarValue() {
+
+
+    const data1 = useSelector(({cars: {cars, searchTerms}}) => {
+
+        const filteredData = cars.filter((car) => {
+            if (!searchTerms) return true;
+            return car.name.toLowerCase().includes(searchTerms.toLowerCase());
+        });
+
+        return filteredData.reduce(function (previous, current) {
+            return previous + parseInt(current.cost);
+        }, 0)
+    });
+
+    const data = [
+        {name: 'car1', cost: 100},
+        {name: 'car2', cost: 200},
+        {name: 'car3', cost: 300},
+        {name: 'car4', cost: 400},
+        {name: 'car5', cost: 500},
+
+    ];
+    const total = data.reduce(function (prev, cur) {
+        return prev + parseInt(cur.cost);
+    }, 0)
+
     return (
-        <div className="bg-purple-50 h-full w-full">
-            <div className="p-2 ">
-
-
-            </div>
+        <div className="">
+            Total Cost:{data1}
         </div>
     )
 }
