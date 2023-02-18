3bf3a30e9672d03d57259d1f4896dc43f501bc16
Anil <anilkumarthakur60@gmail.com>
2023-02-18 09:38:55 +0545

    changes

--- src/store/slice/carsSlice.js
+++ src/store/slice/carsSlice.js
@@ -3,7 +3,28 @@ import {createSlice} from "@reduxjs/toolkit";
 const carsSlice = createSlice({
     name: "car",
     initialState: {
-        cars: [],
+        cars: [
+            {
+                name: "BMW",
+                cost: 100000,
+                id: 1
+            },
+            {
+                name: "BMW1",
+                cost: 1000001,
+                id: 2
+            },
+            {
+                name: "BMW2",
+                cost: 1000002,
+                id: 3
+            },
+            {
+                name: "BMW3",
+                cost: 1000003,
+                id: 4
+            },
+        ],
         searchTerms: null
     },
     reducers: {
