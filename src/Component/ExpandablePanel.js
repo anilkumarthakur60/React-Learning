1b110e036eed6f82f829b6386ee271dd2e14ac62
Anil <anilkumarthakur60@gmail.com>
2023-02-26 01:42:17 +0545

    update

--- src/Component/ExpandablePanel.js
+++ src/Component/ExpandablePanel.js
@@ -1,15 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
 import classNames from "classnames";
+import {GoChevronDown, GoChevronLeft} from "react-icons/go";
+import AlbumList from "../Section21/AlbumList";
 
-function  ExpandablePanel({children,className,...rest}) {
+function ExpandablePanel({header, children,post, ...rest}) {
 
-    const  finalClassName= classNames("border rounded p-3 shadow bg-white w-full",className);
+    const [expanded, setExpanded] = useState(false);
+
+
     return (
-        <div className={finalClassName} {...rest}>
-            {children}
+        <div className={`mb-2 border rounded`}>
+            <div className={`flex p-2 justify-between items-center `}>
+                <div className="flex flex-row justify-between items-center">
+                    {header}
+                </div>
+                <div className="cursor-pointer" onClick={() => setExpanded(!expanded)}>
+
+                    {
+                        expanded ? <GoChevronDown/> :
+                            <GoChevronLeft/>
+                    }
+                </div>
+            </div>
+            <div className="p-2 border-t">
+                {expanded && <div className="p-2 border-t" >
+                    {children}
+                    <AlbumList albums={post.albums}/>
+
+                </div>}
+            </div>
+
         </div>
     );
 
 
 }
-export  default  ExpandablePanel;
+
+export default ExpandablePanel;
--- src/Section21/UserListItem.js
+++ src/Section21/UserListItem.js
@@ -4,6 +4,7 @@ import {GoTrashcan} from "react-icons/go";
 import {useThunk} from "../Hooks/useThunk";
 import {deleteUser} from "../store";
 import Button from "../Component/Button";
+import ExpandablePanel from "../Component/ExpandablePanel";
 
 function UserListItem(props) {
     const {user} = props;
@@ -14,23 +15,37 @@ function UserListItem(props) {
         doDeleteUser(user.id);
     }
 
+    const header=<>
+        <Button loading={isLoadingUser} onClick={handleDeleteUser} className="mr-3">
+            <GoTrashcan/>
+        </Button>
+        {
+            error && <div>{error}</div>
+        }
+        {user.name}
+
+    </>
+
     return (
-        <div className={`mb-2 border rounded`}>
-            <div className={`flex p-2 justify-between items-center cursor-pointer`}>
-                <div className="flex flex-row justify-between items-center">
-
-                    <Button loading={isLoadingUser} onClick={handleDeleteUser} className="mr-3">
-                         <GoTrashcan/>
-                    </Button>
-                    {
-                        error && <div>{error}</div>
-                    }
-
-                    {user.name}
-                </div>
-
-            </div>
-        </div>
+        <ExpandablePanel header={header} post={user}>
+            Content
+        </ExpandablePanel>
+        // <div className={`mb-2 border rounded`}>
+        //     <div className={`flex p-2 justify-between items-center cursor-pointer`}>
+        //         <div className="flex flex-row justify-between items-center">
+        //
+        //             <Button loading={isLoadingUser} onClick={handleDeleteUser} className="mr-3">
+        //                  <GoTrashcan/>
+        //             </Button>
+        //             {
+        //                 error && <div>{error}</div>
+        //             }
+        //
+        //             {user.name}
+        //         </div>
+        //
+        //     </div>
+        // </div>
     );
 }
 
