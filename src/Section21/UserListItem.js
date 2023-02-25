d67ff35ba9f65137487ae0a4a75b62e129269c94
Anil <anilkumarthakur60@gmail.com>
2023-02-26 00:16:09 +0545

    update

--- src/Section21/UserListItem.js
+++ src/Section21/UserListItem.js
@@ -1,34 +1,33 @@
-
 import React from 'react';
 import {GoTrashcan} from "react-icons/go";
 
-import  {useThunk} from "../Hooks/useThunk";
+import {useThunk} from "../Hooks/useThunk";
 import {deleteUser} from "../store";
 import Button from "../Component/Button";
 
 function UserListItem(props) {
     const {user} = props;
 
-    const [doDeleteUser, isLoadingUser,error] = useThunk(deleteUser);
+    const [doDeleteUser, isLoadingUser, error] = useThunk(deleteUser);
 
     const handleDeleteUser = () => {
         doDeleteUser(user.id);
     }
 
     return (
-        <div  className={`mb-2 border rounded`}>
+        <div className={`mb-2 border rounded`}>
             <div className={`flex p-2 justify-between items-center cursor-pointer`}>
-                <Button loading={isLoadingUser}  onClick={handleDeleteUser}>
+                <div className="flex flex-row justify-between items-center">
+
+                    <Button loading={isLoadingUser} onClick={handleDeleteUser} className="mr-3">
+                         <GoTrashcan/>
+                    </Button>
                     {
-                    isLoadingUser? <GoTrashcan className={`animate-spin`}/>:<GoTrashcan />
+                        error && <div>{error}</div>
                     }
 
-                </Button>
-                {
-                    error && <div>{error}</div>
-                }
-
-                {user.name}
+                    {user.name}
+                </div>
 
             </div>
         </div>
