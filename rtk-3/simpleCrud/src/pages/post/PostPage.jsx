3efedb7d3484da20578d566f58ad0ab34cca672a
Anil <anilkumarthakur60@gmail.com>
2023-05-17 10:13:59 +0545

    update

--- rtk-3/simpleCrud/src/pages/post/PostPage.jsx
+++ rtk-3/simpleCrud/src/pages/post/PostPage.jsx
@@ -1,24 +1,23 @@
 import React, { useEffect } from 'react'
 import { useFetchPostsQuery } from "../../redux/post/postApi.js";
 import DataTable from 'react-data-table-component';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import Box from "@mui/material/Box";
 import { LinearProgress } from "@mui/material";
 import { useTableColumn } from "../../hooks/useTableColumn.jsx";
 import { testFunction } from '../../redux/common/commonApi.js';
-import {useTable} from "../../hooks/useTable.js";
+import { useTable } from "../../hooks/useTable.js";
 
 const PostPage = () => {
 
     const { pagination, paginationComponentOptions, progress, filters } = useSelector((state) => state.posts)
     const { data, error, isLoading, refetch } = useFetchPostsQuery({ pagination, filters })
-    const dispatch = useDispatch()
     const { postsColumn } = useTableColumn()
 
 
     const { postStoreData } = testFunction('posts')
 
-    const  {handleSort,handlePerRowsChange,handlePageChange,handleRowSelected}= useTable({refetch:refetch,storeName:'posts'})
+    const { handleSort, handlePerRowsChange, handlePageChange, handleRowSelected } = useTable({ refetch: refetch, storeName: 'posts' })
 
 
 
